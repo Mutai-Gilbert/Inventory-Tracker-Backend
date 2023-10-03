8e26da179679b485acfffdf5d3b4090e0441d801
mutai-gilbert <mutaigilbert85@gmail.com>
2023-10-03 22:28:37 +0300

    Add auth_controller.rb file

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
-    
+    def create
+        @user = User.find_by(username: login_params[:username])
+        if @user && @user.authenticate(login_params[:password])
+            token = encode_token(user_id: @user_id)
+            render json: {user: UserSerializer.new(@user), jwt: token}, status: :accepted
+            
 end
