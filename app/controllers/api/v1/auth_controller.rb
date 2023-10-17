ebd266c9e0b4d98694a07d198de7e96e92a516e7
mutai-gilbert <mutaigilbert85@gmail.com>
2023-10-17 23:21:13 +0300

    Added new changes on the Auth

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
     def create
-        @user = User.find_by(username: login_params[:username])
+        @user = User.find_by(username: login_params[:user])
         if @user && @user.authenticate(login_params[:password])
-            token = encode_token(user_id: @user_id)
+            token = encode_token(user_id: @user.id)
             render json: {user: UserSerializer.new(@user), jwt: token }, status: :accepted
         else
             render json: { error: 'Invalid username or password' }, status: :unauthorized
@@ -16,4 +16,5 @@ class Api::V1::AuthController < ApplicationController
     def login_params
         params.require(:user).permit(:username, :password)
     end
+      
 end
